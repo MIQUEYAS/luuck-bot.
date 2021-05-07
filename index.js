e6ac63a94c8090158518d5933c7cded043d31f65
MIQUEYAS <77650373+MIQUEYAS@users.noreply.github.com>
2021-05-07 20:51:57 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -303,7 +303,7 @@ async function starts() {
 			const mod = [ownerNumber,"12093913386@s.whatsapp.net"]//ubah nomor lo
 			const adminbotnumber = ["12093913386@s.whatsapp.net"]//ubah nomor lo
 			const frendsowner = ["12093913386@s.whatsapp.net"]//ubah nomor lo
-			const premium =["12093913386@s.whatsapp.net","] 
+			const premium =["12093913386@s.whatsapp.net"] 
 			const isGroup = from.endsWith('@g.us')
 			const sender = isGroup ? mek.participant : mek.key.remoteJid
 			const groupMetadata = isGroup ? await client.groupMetadata(from) : ''
